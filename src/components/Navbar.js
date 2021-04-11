1f9c10d276d782c9a62e7b01c91079c7ba857b1f
Taechawat Konkaew <luangta_5@hotmail.com>
2021-04-11 16:23:23 +0700

    Style Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,69 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import React from "react";
+import {
+  createStyles,
+  makeStyles,
+  MuiThemeProvider,
+} from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
-const useStyles = makeStyles((theme) => ({
+const logo = process.env.PUBLIC_URL + "/logo96.png";
+const useStyles = makeStyles((theme) =>
+  createStyles({
     root: {
-        flexGrow: 1,
+      flexGrow: 1,
     },
     menuButton: {
-        marginRight: theme.spacing(2),
+      marginRight: theme.spacing(2),
     },
     title: {
-        flexGrow: 1,
+      flexGrow: 1,
     },
-}));
+    logo: {
+      maxWidth: 40,
+      marginRight: "10px",
+    },
+    button: {
+      margin: theme.spacing(1),
+    },
+  })
+);
 
 export default function Navigator() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography variant="h6" className={classes.title}>
-                        News
-            </Typography>
-                    <Button color="inherit">Github</Button>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <img src={logo} alt="logo" className={classes.logo} />
+          <Typography variant="h6" className={classes.title}></Typography>
+
+          <Button
+            variant="text"
+            color="default"
+            href="https://github.com/luangtatipsy/intel-image-classification"
+            target="_blank"
+            className={classes.button}
+            startIcon={<GitHubIcon />}
+          >
+            github
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
