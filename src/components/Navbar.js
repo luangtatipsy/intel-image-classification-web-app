4619b5b2ba3eccc7e7e6262fa3d5a6a74eef03d1
Taechawat Konkaew <luangta_5@hotmail.com>
2021-04-12 14:24:03 +0700

    Add container to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,6 +11,7 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import GitHubIcon from "@material-ui/icons/GitHub";
+import Container from "@material-ui/core/Container";
 
 const logo = process.env.PUBLIC_URL + "/logo96.png";
 const useStyles = makeStyles((theme) =>
@@ -40,29 +41,31 @@ export default function Navigator() {
   return (
     <div className={classes.root}>
       <AppBar position="static" color="transparent">
-        <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
-          <img src={logo} alt="logo" className={classes.logo} />
-          <Typography variant="h6" className={classes.title}></Typography>
+        <Container fixed>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              className={classes.menuButton}
+              color="inherit"
+              aria-label="menu"
+            >
+              <MenuIcon />
+            </IconButton>
+            <img src={logo} alt="logo" className={classes.logo} />
+            <Typography variant="h6" className={classes.title}></Typography>
 
-          <Button
-            variant="text"
-            color="default"
-            href="https://github.com/luangtatipsy/intel-image-classification"
-            target="_blank"
-            className={classes.button}
-            startIcon={<GitHubIcon />}
-          >
-            github
-          </Button>
-        </Toolbar>
+            <Button
+              variant="text"
+              color="default"
+              href="https://github.com/luangtatipsy/intel-image-classification"
+              target="_blank"
+              className={classes.button}
+              startIcon={<GitHubIcon />}
+            >
+              github
+            </Button>
+          </Toolbar>
+        </Container>
       </AppBar>
     </div>
   );
