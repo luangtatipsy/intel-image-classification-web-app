ae534238f94c1ccbb775d3620b2e4ab564ef143b
Taechawat Konkaew <luangta_5@hotmail.com>
2021-04-15 20:37:22 +0700

    Rename section

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme/MuiTheme";
 import Navigator from "./components/Navbar";
-import MainSection from "./MainSection";
+import Demo from "./DemoSection";
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 
@@ -12,7 +12,7 @@ function App() {
       <MuiThemeProvider theme={theme}>
         <Navigator />
         <Hero />
-        <MainSection />
+        <Demo />
         <Footer />
       </MuiThemeProvider>
     </React.StrictMode>
--- src/DemoSection.js
+++ src/DemoSection.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
+import { Container, Grid, Typography } from "@material-ui/core";
 
-import Gallery from "./components/Gallery";
+// import Gallery from "./components/Gallery";
+import Gallery from "./components/Todo";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -13,6 +13,10 @@ const useStyles = makeStyles((theme) => ({
   container: {
     gridGap: theme.spacing(1),
   },
+  title: {
+    marginBottom: theme.spacing(10),
+    fontWeight: "bold",
+  },
   gallery: {
     // backgroundColor: "red",
   },
@@ -21,11 +25,14 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MainSection() {
+export default function Demo() {
   const classes = useStyles();
   return (
     <div className={classes.root}>
       <Container>
+        <Typography className={classes.title} variant="h4" align="center">
+          LIVE DEMO
+        </Typography>
         <Grid container direction="row">
           <Grid className={classes.gallery} item xs={12} sm={8}>
             <Gallery />
